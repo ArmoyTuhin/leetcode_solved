3ba98d1cfa98590c36f57bec03e75216af7c4c31
Tuhin Armoy <34639924+ArmoyTuhin@users.noreply.github.com>
2022-06-15 23:58:48 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (67.76%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,16 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low =0 , high=n ;
+        while(high-low>1)
+        {
+            int mid = low+(high-low)/2;
+            if(isBadVersion(mid)) high=mid;
+            else low =mid;
+        }
+        return high;
+    }
+};
