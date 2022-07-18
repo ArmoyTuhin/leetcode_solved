525547395fbd1e005c601628afcc58c5d5b8af3c
Tuhin Armoy <34639924+ArmoyTuhin@users.noreply.github.com>
2022-07-19 01:18:18 +0600

    Time: 2 ms (44.56%), Space: 5.7 MB (86.29%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         int low=0 , high=num ;
-        float mid ;
         while(low<=high)
         {
-            mid = low+(high-low)/2;
+           long mid = low+(high-low)/2;
             if(mid*mid==num) return true;
             if(mid*mid < num) low = mid+1 ;
             else high = mid -1;
