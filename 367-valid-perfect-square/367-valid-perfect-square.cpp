551e300743a44c792c18cf9aa04797bbc74ccff8
Tuhin Armoy <34639924+ArmoyTuhin@users.noreply.github.com>
2022-07-19 01:27:28 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (16.80%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -4,7 +4,7 @@ public:
         int low=0 , high=num ;
         while(low<=high)
         {
-           long mid = low+(high-low)/2;
+            long mid = low+(high-low)/2;
             if(mid*mid==num) return true;
             if(mid*mid < num) low = mid+1 ;
             else high = mid -1;
