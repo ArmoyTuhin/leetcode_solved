f841adcf0b6db8dbe39965f8772d7f2c78c073bc
Tuhin Armoy <34639924+ArmoyTuhin@users.noreply.github.com>
2022-08-02 14:13:55 +0600

    Time: 3 ms (74.94%), Space: 6.6 MB (47.79%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -4,11 +4,11 @@ public:
         bool al[26] ={};
         for(int i = 0 ; i<sentence.size() ; i++)
         {
-            al[sentence[i]-'a'] = true ;
+            al[sentence[i]-'a'] = true ; //marking true if a leeter exists
         }
         for(int i= 0 ; i<26 ; i++)
         {
-            if(!al[i]) return false ;
+            if(!al[i]) return false ; //if a letter exists in array or not
         }
         return true;
     }
