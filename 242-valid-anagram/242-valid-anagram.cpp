267bc767086121cfdf3763a938d27f15baf3ebb9
Tuhin Armoy <34639924+ArmoyTuhin@users.noreply.github.com>
2022-08-03 21:56:58 +0600

    Time: 19 ms (36.74%), Space: 7.2 MB (77.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int slen= s.size() , tlen = t.size();
-        if(slen!=tlen) return false;
+        int slen = s.length() , tlen = t.length();
         unordered_map<char, int> mp;
-        for(int i=0 ; i<slen; i++)
+        if(slen!=tlen) return false ;
+        for(int i=0 ; i<slen ; i++)
         {
             mp[s[i]]++;
             mp[t[i]]--;
         }
-        for(auto it: mp)
-        {
+        for(auto it: mp) {
             if(it.second) return false;
         }
         return true;
